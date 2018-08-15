2360559cc93dbf0750cde22d76485bfb243a814b
Zealot <Zealot111@users.noreply.github.com>
2018-08-15 12:48:15 +0300

    rhs update

--- optionals/compat_rhs_afrf3/CfgMagazines.hpp
+++ optionals/compat_rhs_afrf3/CfgMagazines.hpp
@@ -1,10 +1,12 @@
 class cfgMagazines {
     class VehicleMagazine;
-    class rhs_30Rnd_545x39_AK;
+    class rhs_30Rnd_545x39_7N6_AK;
 
-    class rhs_100Rnd_762x54mmR: rhs_30Rnd_545x39_AK {
+    class rhs_100Rnd_762x54mmR: rhs_30Rnd_545x39_7N6_AK
+	{
         ace_isbelt = 1;
-    };
+	};
+
     class rhs_mag_127x108mm_50: VehicleMagazine {
         ace_isbelt = 1;
     };
